8a44f0a654f8fa46443d48d9e87cb172c23b941e
deelaka-24 <1deelakaransilu@gmail.com>
2024-11-15 17:15:02 +0530

    book update,delete done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
   title = 'library-management-frontend';
 }
--- src/app/pages/book-page/book-page.component.ts
+++ src/app/pages/book-page/book-page.component.ts
@@ -2,29 +2,82 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Book } from '../../model/Book';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-page.component.html',
   styleUrls: ['./book-page.component.css'],
 })
 export class BookPageComponent {
-  public bookList: Book[] = [];
+  public bookList: any = [];
 
   constructor(private http: HttpClient) {
-    this.loadBook();
+    this.loadBookTable();
   }
 
-  loadBook() {
+  loadBookTable() {
     this.http
-      .get<Book[]>('http://localhost:8080/book/view-all-books')
-      .subscribe((data) => {
-        this.bookList = data;
+      .get('http://localhost:8080/book/view-all-books')
+      .subscribe((res) => {
+        this.bookList = res;
       });
   }
 
-  updateBook(book: Book) {}
-  deleteBook(id: number) {}
+  deleteBook(id: any) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http
+          .delete(`http://localhost:8080/book/delete-book-by-id/${id}`)
+          .subscribe((data) => {
+            Swal.fire({
+              title: 'Deleted!',
+              text: 'Your file has been deleted.',
+              icon: 'success',
+            });
+            this.loadBookTable();
+          });
+      }
+    });
+    //----------------------
+  }
+
+  public selectedBook: any = {};
+
+  selectBook(book: any) {
+    console.log(book);
+    this.selectedBook = book;
+  }
+
+  updateBook() {
+    Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        this.http
+          .put('http://localhost:8080/book/update-book', this.selectedBook)
+          .subscribe((data) => {
+            Swal.fire('Saved!', '', 'success');
+          });
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info');
+      }
+    });
+  }
 }
